1c4d53b121ee8b77032a89452ae6686a051dbf4d
Kirill Tatarenko <Kirilltatarenko97@gmail.com>
2026-06-03 14:25:32 +0300

    fix api

--- src/dal/api.tsx
+++ src/dal/api.tsx
@@ -1,14 +1,20 @@
-const API_KEY = 'fd817c12-1857-4bc3-9299-bedb266aad8c'
+function apiHeaders(){
+  const apiKey= import.meta.env.VITE_API_KEY
+  if(!apiKey){
+    return undefined
+  }
+  return{'api-key': apiKey}
+}
 
 export function GetTrackDetail(trackId: string) {
   return fetch(
     `https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${trackId}`,
-    { headers: { 'api-key': API_KEY } },
+    { headers: apiHeaders() },
   ).then((res) => res.json())
 }
 
 export function GetTrackList() {
   return fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
-    headers: { 'api-key': API_KEY },
+    headers: apiHeaders(),
   }).then((res) => res.json())
 }
